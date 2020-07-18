8dd120c26f7cb5bca117f812895798adf3912a95
Aditya Patange <adipat247360@gmail.com>
2020-07-18 16:37:11 +0530

    homeScreen: Rearrange code

--- homeScreen.js
+++ homeScreen.js
@@ -2,16 +2,14 @@ const electron = require("electron");
 const { ipcRenderer } = electron;
 
 const btnRentBook = document.querySelector("#btn-rentbook");
+const btnAbout = document.querySelector("#btn-about");
 
 btnRentBook.addEventListener("click", openRentBookWindow);
+btnAbout.addEventListener("click", openAboutWindow);
 
 function openRentBookWindow() {
   ipcRenderer.send("openwindow:rentbook");
 }
-
-const btnAbout = document.querySelector("#btn-about");
-
-btnAbout.addEventListener("click", openAboutWindow);
 
 function openAboutWindow() {
   ipcRenderer.send("openwindow:about");
