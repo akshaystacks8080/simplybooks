0f8cbce12f72734223705c37173be6984b686885
Akshay Pednekar <akshay.pednekar@gmail.com>
2020-07-20 19:53:40 +0530

    rentBooks : get bookId from db and add dblclick handler for list item

--- src/js/rentBook.js
+++ src/js/rentBook.js
@@ -1,5 +1,7 @@
+const electron = require("electron");
 const { Client } = require("pg");
 const { dbconfig } = require("./js/dbconfig");
+const { ipcRenderer } = electron;
 
 const btnSearchBook = document.querySelector("#btn-searchbook");
 
@@ -11,7 +13,7 @@ function displayResults(query) {
   const client = new Client(dbconfig);
   client.connect();
   client.query(
-    `SELECT title FROM books WHERE LOWER(title) LIKE LOWER('%${query}%')`,
+    `SELECT bookid, title FROM books WHERE LOWER(title) LIKE LOWER('%${query}%')`,
     (err, res) => {
       if (res.rows.length == 0) {
         const li = document.createElement("li");
@@ -24,7 +26,9 @@ function displayResults(query) {
         const li = document.createElement("li");
         li.className = "collection-item";
         const textNode = document.createTextNode(row.title);
+        li.setAttribute("bookId", row.bookid);
         li.appendChild(textNode);
+        li.addEventListener("dblclick", showBookOptions);
         searchResultsUl.appendChild(li);
       });
     }
@@ -37,4 +41,10 @@ function searchBook(evt) {
   const searchBar = document.querySelector("#input-search");
   const searchQuery = searchBar.value;
   displayResults(searchQuery);
+}
+
+function showBookOptions(evt) {
+  const bookId = evt.target.getAttribute("bookId");
+  console.log("Clicked book", evt.target.innerHTML, bookId);
+  ipcRenderer.send("openwindow:bookoptions", bookId);
 }
