fa5a6c4314b7848561d23c759d75d803c41ace49
Akshay Pednekar <akshay.pednekar@gmail.com>
2020-07-20 15:56:31 +0530

    rentBook: implemented database query for searching

--- rentBook.js
+++ rentBook.js
@@ -5,18 +5,28 @@ const btnSearchBook = document.querySelector("#btn-searchbook");
 
 btnSearchBook.addEventListener("click", searchBook);
 
-function searchDatabse() {
+async function searchDatabase(query) {
   const result = [];
   const client = new Client(dbconfig);
   client.connect();
+  await client.query(
+    `SELECT title FROM books WHERE LOWER(title) LIKE LOWER('%${query}%')`,
+    (err, res) => {
+      res.rows.forEach((row) => {
+        //console.log(row);
+        result.push(row);
+      });
+    }
+  );
   return result;
 }
 
-function searchBook(evt) {
+async function searchBook(evt) {
   evt.preventDefault();
-  console.log("searcg btn clicked");
+  console.log("search btn clicked");
   const searchBar = document.querySelector("#input-search");
   const searchQuery = searchBar.value;
   console.log(searchQuery);
-  const result = searchDatabse(searchQuery);
+  const result = await searchDatabase(searchQuery);
+  console.log(result);
 }
