2ae67155bfaef4a2d48b00f893cef14c4b59d8cd
Aditya Patange <adipat247360@gmail.com>
2020-07-23 19:37:56 +0530

    viewRentedBooks: Add return book button

--- src/js/viewRentedBooks.js
+++ src/js/viewRentedBooks.js
@@ -4,13 +4,24 @@ const { dbconfig } = require("./js/dbconfig");
 client = new Client(dbconfig);
 client.connect();
 
-client.query("SELECT title FROM rented_books", (err, res) => {
+client.query("SELECT bookid, title FROM rented_books", (err, res) => {
   const rentedBooksUl = document.querySelector("#rentedbooks-list");
   res.rows.forEach((row) => {
+    const returnButton = document.createElement("button");
     const li = document.createElement("li");
+    returnButton.innerHTML = "RETURN";
+    returnButton.className = "btn waves-effect waves-light";
+    returnButton.setAttribute("bookId", row.bookid);
+    returnButton.addEventListener("click", returnBookClick);
     li.className = "collection-item booklist-item";
     const textNode = document.createTextNode(row.title);
     li.appendChild(textNode);
+    li.appendChild(returnButton);
     rentedBooksUl.appendChild(li);
   });
 });
+
+function returnBookClick(evt) {
+  const bookId = evt.target.getAttribute("bookId");
+  console.log("Return book: ", bookId);
+}
